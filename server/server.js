c5d0923ca072aa28f631fb0f99cd3f1e55faed93
Kento75 <kento2github@gmail.com>
2019-11-21 15:40:42 +0900

    Fixed cors settings for development

--- server/server.js
+++ server/server.js
@@ -1,28 +1,37 @@
-const express = require ('express');
-const morgan = require ('morgan');
-const bodyParser = require ('body-parser');
-const cookieParser = require ('cookie-parser');
-const cors = require ('cors');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-require ('dotenv').config ();
+require('dotenv').config();
 
 //// app
-const app = express ();
+const app = express();
 
 //// middlewares
-app.use (morgan ('dev'));
-app.use (bodyParser.json ());
-app.use (cookieParser ());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 // cors
-app.use (cors ());
+if (process.env.NODE_ENV == "development") {
+  app.use(
+    cors({
+      origin: `${process.env.CLIENT_URL}`
+    })
+  );
+}
+
 
 //// routes
-app.get ('/api', (req, res) => {
-  res.json ({time: Date ().toString ()});
+app.get('/api', (req, res) => {
+  res.json({
+    time: Date().toString()
+  });
 });
 
 //// port
 const port = process.env.PORT || 8000;
-app.listen (port, () => {
-  console.log (`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
