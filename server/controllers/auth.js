5d3f1ec990d6cdd9478263d877bf7d398cc8c172
Kento75 <kento2github@gmail.com>
2019-11-22 09:33:53 +0900

    Added create user logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,15 +1,42 @@
-exports.signup = (req, res) => {
-    const {
-        name,
-        email,
-        password
-    } = req.body;
+const User = require('../models/user.js');
+const shortId = require('shortid');
 
-    res.json({
-        user: {
+exports.signup = (req, res) => {
+    User.findOne({
+        email: req.body.email
+    }).exec((err, user) => {
+        // すでに存在する場合
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is token',
+            });
+        }
+
+        const {
             name,
             email,
             password
-        }
+        } = req.body;
+        let username = shortId.generate();
+        let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+        // User作成
+        let newUser = new User({
+            name,
+            email,
+            password,
+            profile,
+            username,
+        });
+        newUser.save((err, success) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err,
+                });
+            }
+            res.json({
+                user: success,
+            });
+        });
     });
-}
+};
