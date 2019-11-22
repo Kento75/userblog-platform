189d42669bb71264b2bd85a2b5e9144d5f5ece4b
Kento Takano <kento2github@gmail.com>
2019-11-22 17:34:51 +0900

    Added Signin component and actions

--- client/actions/auth.js
+++ client/actions/auth.js
@@ -14,4 +14,17 @@ export const signup = (user) => {
   }).then(response => {
     return response.json();
   }).catch((err) => console.log(err));
-}
+};
+
+export const signin = (user) => {
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  }).then(response => {
+    return response.json();
+  }).catch((err) => console.log(err));
+};
