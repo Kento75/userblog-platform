9b7f977578f24562c727ef80c0eae22ae6582bb3
Kento Takano <kento2github@gmail.com>
2019-11-25 07:32:12 +0900

    Added getlist, readOne, remove controller and routes for category api

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const {
-  create
+  create,
+  list,
+  read,
+  remove
 } = require("../controllers/category.js");
 const {
   requireSiginin,
@@ -17,6 +20,11 @@ const {
   categoryCreateValidator
 } = require("../validators/category.js");
 
+
+// routes
 router.post("/category", categoryCreateValidator, runValidation, requireSiginin, adminMiddleware, create);
+router.get("/categories", list);
+router.get("/category/:slug", read);
+router.delete("/category/:slug", requireSiginin, adminMiddleware, remove);
 
 module.exports = router;
