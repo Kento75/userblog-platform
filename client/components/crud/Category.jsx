a3c29bba7f66844f836a9244993171e681b31c84
Kento Takano <kento2github@gmail.com>
2019-11-25 11:12:03 +0900

    Added remove and show message logic for category ui

--- client/components/crud/Category.jsx
+++ client/components/crud/Category.jsx
@@ -34,11 +34,38 @@ const Category = () => {
   const showCategories = () => {
     return categories.map((category, index) => {
       return (
-        <button key={index} className="btn btn-outline-primary mr-1 ml-1 mt-3">
+        <button
+          onDoubleClick={() => deleteConfirm(category.name)}
+          title="Double click to delete"
+          key={index}
+          className="btn btn-outline-primary mr-1 ml-1 mt-3"
+        >
           {category.name}
         </button>
       );
     });
+  };
+
+  const deleteConfirm = slug => {
+    let answer = window.confirm(
+      'Are you sure you want to delete this category?'
+    );
+    if (answer) {
+      removeCategory(slug, token).then(data => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          setValues({
+            ...values,
+            error: false,
+            success: false,
+            name: '',
+            removed: !removed,
+            reload: !reload,
+          });
+        }
+      });
+    }
   };
 
   const clickSubmit = e => {
@@ -68,6 +95,28 @@ const Category = () => {
     });
   };
 
+  const showSuccess = () => {
+    if (success) {
+      return <p className="text-success">Category is created</p>;
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <p className="text-danger">Category already exist</p>;
+    }
+  };
+
+  const showRemoved = () => {
+    if (removed) {
+      return <p className="text-danger">Category is removed</p>;
+    }
+  };
+
+  const mouseMoveHandler = e => {
+    setValues({...values, error: false, success: false, removed: false});
+  };
+
   const newCategoryForm = () => (
     <form onSubmit={clickSubmit}>
       <div className="form-group">
@@ -90,8 +139,13 @@ const Category = () => {
 
   return (
     <React.Fragment>
-      {newCategoryForm()}
-      <div>{showCategories()}</div>
+      {showSuccess()}
+      {showError()}
+      {showRemoved()}
+      <div onMouseMove={mouseMoveHandler}>
+        {newCategoryForm()}
+        {showCategories()}
+      </div>
     </React.Fragment>
   );
 };
