f44e0928d054f2b2fd79eb15790e6f19bd5a14b2
Kento Takano <kento2github@gmail.com>
2019-11-25 10:40:41 +0900

    Added category create ui

--- client/pages/admin/index.jsx
+++ client/pages/admin/index.jsx
@@ -6,7 +6,24 @@ const AdminIndex = () => {
   return (
     <Layout>
       <Admin>
-        <h2>Admin Dashboard</h2>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 pt-5 pb-5">
+              <h2>Admin Dashboard</h2>
+            </div>
+            {/* https://www.w3schools.com/bootstrap4/bootstrap_list_groups.asp */}
+            <div className="col-md-4">
+              <ul class="list-group">
+                <li class="list-group-item list-group-item-action">
+                  <Link href="/admin/crud/category-tag">
+                    <a>Create Category</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-8">right</div>
+          </div>
+        </div>
       </Admin>
     </Layout>
   );
