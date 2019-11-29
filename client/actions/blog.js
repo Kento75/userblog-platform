7c9d5915392dad1676e6daffcbb1d2c8b51d6271
Kento Takano <kento2github@gmail.com>
2019-11-29 15:10:44 +0900

    Added blogs page

--- client/actions/blog.js
+++ client/actions/blog.js
@@ -4,6 +4,10 @@ import {
   API
 } from '../config';
 
+/**
+ * @param  {} blog
+ * @param  {} token
+ */
 export const createBlog = (blog, token) => {
   return fetch(`${API}/blog`, {
       method: 'POST',
@@ -12,6 +16,22 @@ export const createBlog = (blog, token) => {
         Authorization: `Bearer ${token}`
       },
       body: blog
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+/**
+ * 
+ */
+export const listBlogsWithCategoriesAndTags = () => {
+  return fetch(`${API}/blogs-categories-tags`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
     })
     .then(response => {
       return response.json();
