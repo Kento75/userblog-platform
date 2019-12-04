2d0b8285257494c901268fc3825cd349dd36ba77
Kento Takano <kento2github@gmail.com>
2019-12-04 17:26:41 +0900

    Added show tag and categories for update form

--- client/components/crud/BlogUpdate.jsx
+++ client/components/crud/BlogUpdate.jsx
@@ -13,7 +13,12 @@ import {QuillFormats, QuillModules} from '../../helpers/quill';
 const ReactQuill = dynamic(() => import('react-quill'), {ssr: false});
 
 const BlogUpdate = ({router}) => {
-  const [body, setBody] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [checkedCategory, setCheckedCategory] = useState([]); // categories
+  const [checkedTag, setCheckedTag] = useState([]); //tags
+
+  const [body, setBody] = useState(''); // blog
   const [values, setValues] = useState({
     error: '',
     success: '',
@@ -27,7 +32,31 @@ const BlogUpdate = ({router}) => {
   useEffect(() => {
     setValues({...values, formData: new FormData()});
     initBlog();
+    initCategories();
+    initTags();
   }, [router]);
+
+  // カテゴリ一覧取得
+  const initCategories = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        setValues({...values, error: data.error});
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  // タグ一覧取得
+  const initTags = () => {
+    getTags().then(data => {
+      if (data.error) {
+        setValues({...values, error: data.error});
+      } else {
+        setTags(data);
+      }
+    });
+  };
 
   const initBlog = () => {
     if (router.query.slug) {
@@ -37,9 +66,107 @@ const BlogUpdate = ({router}) => {
         } else {
           setValues({...values, title: data.title});
           setBody(data.body);
+          setCategoriesArray(data.categories);
+          setTagsArray(data.tags);
         }
       });
     }
+  };
+
+  // ブログからカテゴリ一覧を生成
+  const setCategoriesArray = blogCategories => {
+    let categoriesArray = [];
+    blogCategories.map((category, index) => {
+      categoriesArray.push(category._id);
+    });
+
+    setCheckedCategory(categoriesArray);
+  };
+
+  // ブログからタグ一覧を生成
+  const setTagsArray = blogTags => {
+    let tagsArray = [];
+    blogTags.map((tag, index) => {
+      tagsArray.push(tag._id);
+    });
+    setCheckedTag(tagsArray);
+  };
+
+  // カテゴリ一覧表示コンポーネント
+  const showCategories = () => {
+    return (
+      categories &&
+      categories.map((category, index) => (
+        <li key={index} className="list-unstyled">
+          <input
+            onChange={handleCategoriesToggle(category._id)}
+            checked={findOutCategory(category._id)}
+            type="checkbox"
+            className="mr-2"
+          />
+          <label className="form-check-label">{category.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  // タグ一覧表示コンポーネント
+  const showTags = () => {
+    return (
+      tags &&
+      tags.map((tag, index) => (
+        <li key={index} className="list-unstyled">
+          <input
+            onChange={handleTagsToggle(tag._id)}
+            checked={findOutTag(tag._id)}
+            type="checkbox"
+            className="mr-2"
+          />
+          <label className="form-check-label">{tag.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  // 既に洗濯済みである場合checked
+  // arg -> category._id
+  const findOutCategory = category =>
+    checkedCategory.indexOf(category) ? true : false;
+
+  // 既に洗濯済みである場合checked
+  // arg -> tag._id
+  const findOutTag = tag => (checkedTag.indexOf(tag) ? true : false);
+
+  // カテゴリトグル
+  const handleCategoriesToggle = c => () => {
+    setValues({...values, error: ''});
+    // return thhe first index or -1
+    const clickedCategory = checkedCategory.indexOf(c);
+    const all = [...checkedCategory];
+
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    setCheckedCategory(all);
+    formData.set('categories', all);
+  };
+
+  // タグトグル
+  const handleTagsToggle = t => () => {
+    setValues({...values, error: ''});
+    // return thhe first index or -1
+    const clickedTag = checkedTag.indexOf(t);
+    const all = [...checkedTag];
+
+    if (clickedTag === -1) {
+      all.push(t);
+    } else {
+      all.splice(clickedTag, 1);
+    }
+    setCheckedTag(all);
+    formData.set('tags', all);
   };
 
   const handleChange = name => e => {
@@ -92,15 +219,41 @@ const BlogUpdate = ({router}) => {
       <div className="row">
         <div className="col-md-8 text-break">
           {updateBlogForm()}
-          <div className="pt-3">show success and error msg</div>
+          <div className="pt-3">
+            <p>show success and error msg</p>
+            <hr />
+            {JSON.stringify(categories)}
+            <hr />
+            {JSON.stringify(tags)}
+          </div>
         </div>
 
         <div className="col-md-4">
-          <div>
-            <div className="form-group pb-2">
-              <h5>Featured image</h5>
-            </div>
+          <div className="form-group pb-2">
+            <h5>Featured image</h5>
+            <hr />
+            <small className="text-muted">Max size: 1mb</small>
+            <br />
+            <label className="btn btn-outline-info">
+              Upload featured image
+              <input
+                onChange={handleChange('photo')}
+                type="file"
+                accept="image/*"
+                hidden
+              />
+            </label>
           </div>
+          <h5>Categories</h5>
+          <hr />
+          <ul style={{maxHeight: '200px', overflowY: 'scroll'}}>
+            {showCategories()}
+          </ul>
+          <h5>Tags</h5>
+          <hr />
+          <ul style={{maxHeight: '200px', overflowY: 'scroll'}}>
+            {showTags()}
+          </ul>
         </div>
       </div>
     </div>
