9013b31fc6a6880bd12c42c0d3e8ebb77d09579a
Kento Takano <kento2github@gmail.com>
2019-12-11 09:04:55 +0900

    Added next.config.js for samplefile

---
+++ client/next.config.sample.js
@@ -0,0 +1,15 @@
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
+  publicRuntimeConfig: {
+    APP_NAME: "UserBlog",
+    API_PRODUCTION: "https://userblog.com",
+    API_DEVELOPMENT: "http://localhost:8000/api",
+    PRODUCTION: false,
+    DOMAIN_DEVELOPMENT: "http://localhost:3000",
+    DOMAIN_PRODUCTION: "https://userblog.com",
+    FB_APP_ID: "", // 僕は使わない FacebookのAPP_ID
+    DISQUS_SHORTNAME: 'userblog-1',
+    GOOGLE_CLIENT_ID: 'Your google console client id'
+  }
+});
